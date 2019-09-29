33127cfa702aff2e19a5b5fa4bc0c0d407e67ab3
NightNMare <dlarbdnjs1@naver.com>
2019-09-30 00:15:58 +0900

    Black, auto next day

--- app/src/main/java/com/naver/nightnmare/lunch_for_sunrin2/MainActivity.java
+++ app/src/main/java/com/naver/nightnmare/lunch_for_sunrin2/MainActivity.java
@@ -2,10 +2,13 @@ package com.naver.nightnmare.lunch_for_sunrin2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import com.google.gson.JsonArray;
@@ -14,6 +17,7 @@ import com.google.gson.JsonObject;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 
 import retrofit2.Call;
@@ -27,12 +31,15 @@ public class MainActivity extends AppCompatActivity {
 
     long mNow;
     Date mDate;
-    SimpleDateFormat mFormat = new SimpleDateFormat("yyyyMMdd");
+    SimpleDateFormat mFormat = new SimpleDateFormat("yyyyMMddHH");
     TextView tv;
     Button btn;
+    Switch isBlack;
 
     String current_time;
     String menus="";
+    SharedPreferences prefs;
+    SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +47,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         tv = findViewById(R.id.tv);
         btn = findViewById(R.id.button);
+        isBlack = findViewById(R.id.isBlack);
+        prefs = getSharedPreferences("isBlack",MODE_PRIVATE);
+        editor= prefs.edit();
+
+        if(prefs.getBoolean("isBlack",false)){
+            isBlack.setChecked(true);
+            isBlack.setText("Black");
+        }
+
+        isBlack.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(b){
+                    isBlack.setText("Black");
+                    editor.putBoolean("isBlack",true);
+                }else{
+                    isBlack.setText("White");
+                    editor.putBoolean("isBlack",false);
+                }
+                editor.apply();
+            }
+        });
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -50,14 +79,29 @@ public class MainActivity extends AppCompatActivity {
         });
         getData();
     }
+    int day;
+    int last_day_of_month;
+    int month;
+
     private void getData(){
         mNow = System.currentTimeMillis();
         mDate = new Date(mNow);
         current_time = mFormat.format(mDate);
-        menus=current_time.substring(0,4)+"."+current_time.substring(4,6)+"."+current_time.substring(6,8)+"\n\n";
 
-        Call<JsonObject> res = NetRetrofit.getInstance().getService().getData("high","B100000658",current_time.substring(4,6),false);
-//        Call<JsonObject> res = NetRetrofit.getInstance().getService().getData("high","B100000658","09",false);
+
+        day = Integer.parseInt(current_time.substring(6,8))-1;
+        month = Integer.parseInt(current_time.substring(4,6));
+        last_day_of_month = Calendar.getInstance().getActualMaximum(Calendar.DAY_OF_MONTH);
+
+        if(Integer.parseInt(current_time.substring(8,10))>15){ //3시 넘으면
+            day++;//최대가 마지막날짜
+            if(day > last_day_of_month){//3시가 넘고 오늘이 월 마지막 날이면
+                month++;
+                day=0;
+            }
+        }
+
+        Call<JsonObject> res = NetRetrofit.getInstance().getService().getData("high","B100000658",month+"",false);
 
         res.enqueue(new Callback<JsonObject>() {
             @Override
@@ -67,9 +111,17 @@ public class MainActivity extends AppCompatActivity {
                     JsonElement datas = response.body().get("menu");
                     JsonArray root = datas.getAsJsonArray();
 
-                    JsonObject jsonObject = root.get(Integer.parseInt(current_time.substring(6,8))-1).getAsJsonObject();
-//                    JsonObject jsonObject = root.get(26).getAsJsonObject();
+                    //오늘 급식이 없으면
+                    while(root.get(day).getAsJsonObject().get("lunch").getAsJsonArray().size()==0){
+                        day++;
+                        if(day > last_day_of_month){//오늘이 월 마지막날이면
+                            month++;
+                            day=0;
+                        }
+                    }
+                    JsonObject jsonObject = root.get(day).getAsJsonObject();
 
+                    menus=current_time.substring(0,4)+"."+month+"."+(day+1)+"\n\n";
                     Data data = new Data(jsonObject.get("date").getAsString(),jsonObject.get("breakfast").getAsJsonArray(),jsonObject.get("lunch").getAsJsonArray(),jsonObject.get("dinner").getAsJsonArray());
                     for(int i=0;i<data.getLunch().size();i++){
                         menus+=data.getLunch().get(i).getAsString()+"\n\n";
--- app/src/main/java/com/naver/nightnmare/lunch_for_sunrin2/NewAppWidget.java
+++ app/src/main/java/com/naver/nightnmare/lunch_for_sunrin2/NewAppWidget.java
@@ -6,6 +6,8 @@ import android.appwidget.AppWidgetProvider;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Handler;
 import android.util.Log;
 import android.widget.RemoteViews;
@@ -16,6 +18,7 @@ import com.google.gson.JsonObject;
 
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 import retrofit2.Call;
@@ -30,6 +33,7 @@ public class NewAppWidget extends AppWidgetProvider {
     static String menu;
     private static final String ACTION_BUTTON1 = "com.example.gyu_won.lunch_for_sunrin.Refresh";
     static String current_time;
+    static SharedPreferences prefs;
 
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
@@ -37,13 +41,20 @@ public class NewAppWidget extends AppWidgetProvider {
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.new_app_widget);
 
         views.setTextViewText(R.id.appwidget_text, menu);
-
+        prefs = context.getSharedPreferences("isBlack", Context.MODE_PRIVATE);
+        if(prefs.getBoolean("isBlack",false)){
+            views.setTextColor(R.id.appwidget_text, Color.BLACK);
+            views.setTextColor(R.id.button,Color.BLACK);
+        }else{
+            views.setTextColor(R.id.appwidget_text, Color.WHITE);
+            views.setTextColor(R.id.button,Color.WHITE);
+        }
 
         Intent intentSync = new Intent(context, NewAppWidget.class);
         intentSync.setAction(ACTION_BUTTON1); //You need to specify the action for the intent. Right now that intent is doing nothing for there is no action to be broadcasted.
 
-        PendingIntent pendingSync = PendingIntent.getBroadcast(context,0, intentSync, PendingIntent.FLAG_UPDATE_CURRENT); //You need to specify a proper flag for the intent. Or else the intent will become deleted.
-        views.setOnClickPendingIntent(R.id.button,pendingSync);
+        PendingIntent pendingSync = PendingIntent.getBroadcast(context, 0, intentSync, PendingIntent.FLAG_UPDATE_CURRENT); //You need to specify a proper flag for the intent. Or else the intent will become deleted.
+        views.setOnClickPendingIntent(R.id.button, pendingSync);
 
         appWidgetManager.updateAppWidget(appWidgetId, views);
     }
@@ -63,8 +74,8 @@ public class NewAppWidget extends AppWidgetProvider {
         int[] appWidgets = appWidgetManager.getAppWidgetIds(thisAppWidget);
 
 //        String action = intent.getAction();
-        if(intent.getAction().equals(ACTION_BUTTON1)){
-            onUpdate(context,appWidgetManager,appWidgets);
+        if (intent.getAction().equals(ACTION_BUTTON1)) {
+            onUpdate(context, appWidgetManager, appWidgets);
         }
         super.onReceive(context, intent);
     }
@@ -78,6 +89,7 @@ public class NewAppWidget extends AppWidgetProvider {
     public void onDisabled(Context context) {
         // Enter relevant functionality for when the last widget is disabled
     }
+
     private static class update {
         Context context;
         AppWidgetManager appWidgetManager;
@@ -97,25 +109,41 @@ public class NewAppWidget extends AppWidgetProvider {
                 for (int appWidgetId : appWidgetIds) {
                     updateAppWidget(context, appWidgetManager, appWidgetId);
                 }
-                menu ="";
+                menu = "";
                 handler.post(new Runnable() {
                     @Override
                     public void run() {
-                        getData(context,appWidgetManager,appWidgetIds);
+                        getData(context, appWidgetManager, appWidgetIds);
                     }
                 });
             }
         };
     }
-    private static void getData(final Context context, final AppWidgetManager appWidgetManager, final int[] appWidgetIds){
-        SimpleDateFormat mFormat = new SimpleDateFormat("yyyyMMdd");
-        long mNow= System.currentTimeMillis();
-        Date mDate = new Date(mNow);;
-        current_time = mFormat.format(mDate);
-        menu=current_time.substring(0,4)+"."+current_time.substring(4,6)+"."+current_time.substring(6,8)+"\n\n";
 
-        Call<JsonObject> res = NetRetrofit.getInstance().getService().getData("high","B100000658",current_time.substring(4,6),true);
-//        Call<JsonObject> res = NetRetrofit.getInstance().getService().getData("high","B100000658","09",true);
+    static int day = 0;
+    static int last_day_of_month = 0;
+    static int month = 0;
+
+    private static void getData(final Context context, final AppWidgetManager appWidgetManager, final int[] appWidgetIds) {
+        SimpleDateFormat mFormat = new SimpleDateFormat("yyyyMMddHH");
+        long mNow = System.currentTimeMillis();
+        Date mDate = new Date(mNow);
+        current_time = mFormat.format(mDate);
+
+
+        day = Integer.parseInt(current_time.substring(6, 8)) - 1;
+        month = Integer.parseInt(current_time.substring(4, 6));
+        last_day_of_month = Calendar.getInstance().getActualMaximum(Calendar.DAY_OF_MONTH);
+
+        if (Integer.parseInt(current_time.substring(8, 10)) > 15) { //3시 넘으면
+            day++;//최대가 마지막날짜
+            if (day > last_day_of_month) {//3시가 넘고 오늘이 월 마지막 날이면
+                month++;
+                day = 0;
+            }
+        }
+
+        Call<JsonObject> res = NetRetrofit.getInstance().getService().getData("high", "B100000658", month + "", true);
 
         res.enqueue(new Callback<JsonObject>() {
             @Override
@@ -125,12 +153,20 @@ public class NewAppWidget extends AppWidgetProvider {
                     JsonElement datas = response.body().get("menu");
                     JsonArray root = datas.getAsJsonArray();
 
-                    JsonObject jsonObject = root.get(Integer.parseInt(current_time.substring(6,8))-1).getAsJsonObject();
-//                    JsonObject jsonObject = root.get(26).getAsJsonObject();
+                    //오늘 급식이 없으면
+                    while (root.get(day).getAsJsonObject().get("lunch").getAsJsonArray().size() == 0) {
+                        day++;
+                        if (day > last_day_of_month) {//오늘이 월 마지막날이면
+                            month++;
+                            day = 0;
+                        }
+                    }
+                    JsonObject jsonObject = root.get(day).getAsJsonObject();
 
-                    Data data = new Data(jsonObject.get("date").getAsString(),jsonObject.get("breakfast").getAsJsonArray(),jsonObject.get("lunch").getAsJsonArray(),jsonObject.get("dinner").getAsJsonArray());
-                    for(int i=0;i<data.getLunch().size();i++){
-                        menu+=data.getLunch().get(i).getAsString()+"\n";
+                    menu = current_time.substring(0, 4) + "." + month + "." + (day + 1) + "\n\n";
+                    Data data = new Data(jsonObject.get("date").getAsString(), jsonObject.get("breakfast").getAsJsonArray(), jsonObject.get("lunch").getAsJsonArray(), jsonObject.get("dinner").getAsJsonArray());
+                    for (int i = 0; i < data.getLunch().size(); i++) {
+                        menu += data.getLunch().get(i).getAsString() + "\n";
                     }
                     for (int appWidgetId : appWidgetIds) {
                         updateAppWidget(context, appWidgetManager, appWidgetId);
@@ -138,6 +174,7 @@ public class NewAppWidget extends AppWidgetProvider {
 
                 }
             }
+
             @Override
             public void onFailure(Call<JsonObject> call, Throwable t) {
                 Log.e("Err", t.getMessage());
